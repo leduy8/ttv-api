cfd3d216b6579d6e3ae5474260daa29ba7f2c0ed
Nghia <d9ltnghia@cmcglobal.vn>
2022-10-17 13:39:02 +0700

    fix: Fix OAuth flow and return data

--- main/controllers/google_oauth.py
+++ main/controllers/google_oauth.py
@@ -6,8 +6,8 @@ from main.commons.decorators import validate_data
 from main.commons.error_handlers import InternalServerError
 from main.engines.user import create_user_google, get_user_by_email
 from main.schemas.google import GoogleSchema
-from main.schemas.user import FullUserInfoSchema
 from main.services.redis import redis_client
+from main.utils.jwt import create_access_token
 
 
 @app.post("/google/login")
@@ -17,7 +17,7 @@ def google_login(data: dict):
     user = get_user_by_email(user["email"])
 
     if user:
-        return FullUserInfoSchema.dumps(user)
+        return jsonify({"access_token": create_access_token({"email": user.email})})
 
     if not google.authorized:
         return redirect(url_for("google.login"))
@@ -33,11 +33,20 @@ def google_login(data: dict):
 
     if not get_user_by_email(user_info["email"]):
         user = create_user_google(user_info)
+    else:
+        user = get_user_by_email(user_info["email"])
 
     # ? Save only email to redis to save space, because user's data is saved in DB
-    redis_client.set(user_info["id"], user_info["email"])
+    redis_client.set(
+        current_app.blueprints["google"].token, user_info["email"], ex=604800
+    )  # ? 604800 = 1 week
 
-    return FullUserInfoSchema.dumps(user)
+    return jsonify(
+        {
+            "access_token": create_access_token({"email": user.email}),
+            "google_token": current_app.blueprints["google"].token,
+        }
+    )
 
 
 @app.get("/google/logout")
@@ -45,4 +54,4 @@ def google_login(data: dict):
 def google_logout(data: dict):
     del current_app.blueprints["google"].token
     redis_client.delete(data["google_token"])
-    return jsonify("Logged out")
+    return jsonify({"message": "Logged out"})
